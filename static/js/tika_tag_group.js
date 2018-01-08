6d05a4d1150bb25525de24026b590b10380eb65c
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-01-08 18:05:32 -0300

    tika eng

---
+++ static/js/tika_tag_group.js
@@ -0,0 +1,63 @@
+function isLocalHost() {
+    return document.location.href.indexOf('localhost') != -1;
+}
+
+function isDevelopment() {
+    return document.location.href.indexOf('ondev.today') != -1;
+}
+
+
+$(document).ready(function()
+{
+    var base_url = $.environmentVar(
+        "https://apibodegas.loadingplay.com/",
+        "https://apibodegas.loadingplay.com/",
+        "https://apibodegas.loadingplay.com/"
+    );
+    var app_public = $.environmentVar(10,10,10);
+
+    var config = {
+        'app_public' : app_public,
+        'base_url' : base_url,
+        'maxProducts' : 6,
+        'templateOrigin' : '#product-box',
+        'tag' : 'tika%20chip',
+        'ignore_stock' : true,
+        'onLoad':function(){
+            $('.ellipsis').ellipsis();
+        }
+    };
+
+    // tika chip
+    $('.tika-chip').ecommerce('product_box', config);
+
+    // tika sticks
+    config.tag = 'tika%20sticks';
+    $('.tika-sticks').ecommerce('product_box', config);
+
+    // tika tortikas
+    config.tag = 'tika%20tortikas';
+    $('.tika-tortikas').ecommerce('product_box', config);
+
+    // tika crakers
+    config.tag = 'tika%20crakers';
+    $('.tika-crakers').ecommerce('product_box', config);
+
+    // tika nuts
+    config.tag = 'tika%20nuts';
+    $('.tika-nuts').ecommerce('product_box', config);
+
+    // tika home
+    config.tag = 'tika%20home';
+    $('.tika-home').ecommerce('product_box', config);
+
+    // tika veggies
+    config.tag = 'Tika%20Veggies';
+    $('.tika-veggies').ecommerce('product_box', config);
+
+    // tika pop
+    config.tag = 'Tika%20pop';
+    $('.tika-pop').ecommerce('product_box', config);
+
+
+});
